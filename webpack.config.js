8ef81623c8298e9ad1b45ccc76909436ea3b001f
Emiliano <e.tortorelli1991@gmail.com>
2018-10-16 11:39:40 -0300

    working site

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require("path");
+const glob = require("glob");
+var webpack = require("webpack");
 
 module.exports = {
-    mode: "development" ,
+    mode: "development",
     optimization: {
         // We no not want to minimize our code.
         minimize: false
     },
-    entry: "./src/js/index.js",
+    entry: glob.sync("./src/js/main.js"),
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js"
     },
     module: {
+        // loaders: [
+        //     {
+        //         test: /[\/\\]node_modules[\/\\]some-module[\/\\]index\.js$/,
+        //         loader: "imports-loader?this=>window"
+        //     }
+        // ],
         rules: [
             {
                 test: /\.js$/,
@@ -22,7 +30,27 @@ module.exports = {
                         presets: ["@babel/preset-env"]
                     }
                 }
+            },
+            {
+                test: /\.(css|scss)$/,
+                loaders: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
             }
         ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
+    resolve: {
+        alias: {
+            jquery: "jquery/src/jquery"
+        }
     }
+
 };
